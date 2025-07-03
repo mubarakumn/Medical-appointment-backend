fc8c7b0d9d041390533466ea2267906a163e22a3
mubarak usman <mubarak725908@gmail.com>
2025-07-03 07:36:27 +0100

    update

--- Routes/AppointmentRoute.js
+++ Routes/AppointmentRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { bookAppointment, getMyAppointments, updateAppointment, cancelAppointment } = require('../Controllers/appointmentController');
+const { bookAppointment, getMyAppointments, updateAppointment, cancelAppointment } = require('../Controllers/appointmentCtrl');
 const authMiddleware = require('../MiddleWares/authMiddleware');
 
 const router = express.Router();
